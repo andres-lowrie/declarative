4c55f773ba89bf3fa1e1843506e4188093655711
Andres Lowrie <andres.lowrie@gmail.com>
2022-12-29 15:37:07 -0600

    feat: add underscore prefix for function constructor

--- index.js
+++ index.js
@@ -1,43 +1,64 @@
 // Identity
 exports.id = a => a
 
+// Composition
+// constructs a function. Useful for lazy like looping
+exports.make = (...args) => {
+  const [fn, ...bargs] = args
+  return () => fn(...bargs)
+}
+
 // No-Operation
 // In JS land the closest we can get to "do nothing" is returning undefined
 exports.noop = (...args) => undefined
 
 // Equals
 exports.eq = (a, b) => a === b
+exports._eq = (a, b) => exports.make(exports.eq, a, b)
 
 // Greater than
 exports.gt = (a, b) => a > b
+exports._gt = (a, b) => exports.make(exports.gt, a, b)
 
 // Greater than or equals
 exports.gte = (a, b) => a >= b
+exports._gte = (a, b) => exports.make(exports.gte, a, b)
 
 // Less than
 exports.lt = (a, b) => a < b
+exports._lt = (a, b) => exports.make(exports.lt, a, b)
 
 // Less than or equal
 exports.lte = (a, b) => a <= b
+exports._lte = (a, b) => exports.make(exports.lte, a, b)
 
 // Expose Object methods
 exports.keys = a => Object.keys(a)
+exports._keys = a => exports.make(exports.keys, a)
 
 exports.values = a => Object.values(a)
+exports._values = a => exports.make(exports.values, a)
 
 exports.entries = a => Object.entries(a)
+exports._entries = a => exports.make(exports.entries, a)
 
 exports.pairs = exports.entries
+exports._pairs = exports._entries
 
 exports.toEntries = exports.entries
+exports._toEntries = exports._entries
 
 exports.toPairs = exports.entries
+exports._toPairs = exports._entries
 
 exports.fromEntries = a => Object.fromEntries(a)
+exports._fromEntries = a => exports.make(exports.fromEntries, a)
 
 exports.fromPairs = a => Object.fromEntries(a)
+exports._fromPairs = a => exports.make(exports.fromPairs, a)
 
 exports.assign = Object.assign
+exports._assign = (...args) => exports.make(exports.assign, ...args)
 
 exports.mkMerge = (mutate = false) => (...args) => {
   if (args.every(exports.isObject)) {
@@ -58,32 +79,39 @@ exports.mkMerge = (mutate = false) => (...args) => {
     }
   }
 }
+exports._mkMerge = (mutate = false) => (...args) =>
+  exports.make(exports.mkMerge(mutate), ...args)
 
 exports.merge = exports.mkMerge(true)
-exports.impureMerge = exports.merge
-exports.pureMerge = exports.mkMerge(false)
+exports._merge = exports._mkMerge(true)
 
-// Composition
-exports.make = (...args) => {
-  const [fn, ...bargs] = args
-  return () => fn(...bargs)
-}
+exports.impureMerge = exports.merge
+exports._impureMerge = exports._merge
+
+exports.pureMerge = exports.mkMerge(false)
+exports._pureMerge = exports._mkMerge(false)
 
 // Comparison
 exports.isTrue = a => a === true
 exports._isTrue = a => exports.make(exports.isTrue, a)
 
 exports.isFalse = a => a === false
+exports._isFalse = a => exports.make(exports.isFalse, a)
 
 exports.isFunction = a => typeof a === 'function'
+exports._isFunction = a => exports.make(exports.isFunction, a)
 
 exports.isArray = Array.isArray
+exports._isArray = (...args) => exports.make(exports.isArray, ...args)
 
 exports.isString = a => typeof a === 'string'
+exports._isString = a => exports.make(exports.isString, a)
 
 exports.isNumber = a => typeof a === 'number'
+exports._isNumber = a => exports.make(exports.isNumber, a)
 
 exports.isNull = a => a === null
+exports._isNull = a => exports.make(exports.isNull, a)
 
 exports.length = a => {
   let arr = []
@@ -97,16 +125,22 @@ exports.length = a => {
 
   return arr.length
 }
+exports._length = a => exports.make(exports.length, a)
 
 // @TODO add more types checks
 exports.isObject = a =>
   typeof a === 'object' && !exports.isNull(a) && !exports.isArray(a)
 
+exports._isObject = a => exports.make(exports.isObject, a)
+
 exports.isSymbol = a => typeof a === 'symbol'
+exports._isSymbol = a => exports.make(exports.isSymbol, a)
 
 exports.isUndefined = a => typeof a === 'undefined'
+exports._isUndefined = a => exports.make(exports.isUndefined, a)
 
 exports.isDefined = a => !exports.isUndefined(a)
+exports._isDefined = a => exports.make(exports.isDefined, a)
 
 // Access
 exports.first = a => {
@@ -118,6 +152,7 @@ exports.first = a => {
     return exports.first(exports.pairs(a))
   }
 }
+exports._first = a => exports.make(exports.first, a)
 
 exports.last = a => {
   if (exports.isArray(a)) {
@@ -128,9 +163,12 @@ exports.last = a => {
     return exports.last(exports.pairs(a))
   }
 }
+exports._last = a => exports.make(exports.last, a)
 
 // Checks
 
 exports.isEmpty = a => exports.length(a) === 0
+exports._isEmpty = a => exports.make(exports.isEmpty, a)
 
 exports.isNotEmpty = a => !exports.isEmpty(a)
+exports._isNotEmpty = a => exports.make(exports.isNotEmpty, a)
--- tests/main.js
+++ tests/main.js
@@ -23,11 +23,11 @@ const cases = [
   }),
 
   // Composition
-  test('make: It should return a function', () =>{
+  test('make: It should return a function', () => {
     const got = mod.make('foo', 'bar')
     assert.strictEqual(typeof got, 'function')
   }),
-  test('make: It should pass the rest of the arguments to the function it returns', () =>{
+  test('make: It should pass the rest of the arguments to the function it returns', () => {
     const state = []
     const fn = (...args) => state.push(...args)
 
@@ -49,6 +49,13 @@ const cases = [
     const got = mod.eq(op1, op2)
     assert.strictEqual(got, want)
   }),
+  test('_eq', () => {
+    const op1 = 1
+    const op2 = 2
+    const want = false
+    const got = mod._eq(op1, op2)
+    assert.strictEqual(got(), want)
+  }),
 
   test('gt: It should compare primitives', () => {
     const op1 = 1
@@ -56,6 +63,13 @@ const cases = [
     const want = false
     const got = mod.gt(op1, op2)
     assert.strictEqual(got, want)
+  }),
+  test('_gt', () => {
+    const op1 = 1
+    const op2 = 2
+    const want = false
+    const got = mod._gt(op1, op2)
+    assert.strictEqual(got(), want)
   }),
 
   test('gte: It should compare primitives', () => {
@@ -65,6 +79,13 @@ const cases = [
     const got = mod.gte(op1, op2)
     assert.strictEqual(got, want)
   }),
+  test('_gte', () => {
+    const op1 = 1
+    const op2 = 2
+    const want = false
+    const got = mod._gte(op1, op2)
+    assert.strictEqual(got(), want)
+  }),
 
   test('lt: It should compare primitives', () => {
     const op1 = 1
@@ -72,6 +93,13 @@ const cases = [
     const want = true
     const got = mod.lt(op1, op2)
     assert.strictEqual(got, want)
+  }),
+  test('_lt', () => {
+    const op1 = 1
+    const op2 = 2
+    const want = true
+    const got = mod._lt(op1, op2)
+    assert.strictEqual(got(), want)
   }),
 
   test('lte: It should compare primitives', () => {
@@ -81,6 +109,13 @@ const cases = [
     const got = mod.lte(op1, op2)
     assert.strictEqual(got, want)
   }),
+  test('_lte', () => {
+    const op1 = 1
+    const op2 = 2
+    const want = true
+    const got = mod._lte(op1, op2)
+    assert.strictEqual(got(), want)
+  }),
 
   // Object methods
   test('keys: It should call Object.keys', () => {
@@ -89,12 +124,24 @@ const cases = [
     const got = mod.keys(given)
     assert.deepEqual(got, want)
   }),
+  test('_keys', () => {
+    const given = { a: 'foo', b: 'bar' }
+    const want = Object.keys(given)
+    const got = mod._keys(given)
+    assert.deepEqual(got(), want)
+  }),
 
   test('values: It should call Object.values', () => {
     const given = { a: 'foo', b: 'bar' }
     const want = Object.values(given)
     const got = mod.values(given)
     assert.deepEqual(got, want)
+  }),
+  test('_values', () => {
+    const given = { a: 'foo', b: 'bar' }
+    const want = Object.values(given)
+    const got = mod._values(given)
+    assert.deepEqual(got(), want)
   }),
 
   test('entries: It should call Object.entries', () => {
@@ -103,12 +150,24 @@ const cases = [
     const got = mod.entries(given)
     assert.deepEqual(got, want)
   }),
+  test('_entries', () => {
+    const given = { a: 'foo', b: 'bar' }
+    const want = Object.entries(given)
+    const got = mod._entries(given)
+    assert.deepEqual(got(), want)
+  }),
 
   test('pairs: Is an alias for entries', () => {
     const given = { a: 'foo', b: 'bar' }
     const want = Object.entries(given)
     const got = mod.pairs(given)
     assert.deepEqual(got, want)
+  }),
+  test('_pairs', () => {
+    const given = { a: 'foo', b: 'bar' }
+    const want = Object.entries(given)
+    const got = mod._pairs(given)
+    assert.deepEqual(got(), want)
   }),
 
   test('toEntries: Is an alias for entries', () => {
@@ -117,12 +176,24 @@ const cases = [
     const got = mod.toEntries(given)
     assert.deepEqual(got, want)
   }),
+  test('_toEntries', () => {
+    const given = { a: 'foo', b: 'bar' }
+    const want = Object.entries(given)
+    const got = mod._toEntries(given)
+    assert.deepEqual(got(), want)
+  }),
 
   test('toPairs: Is an alias for entries', () => {
     const given = { a: 'foo', b: 'bar' }
     const want = Object.entries(given)
     const got = mod.toPairs(given)
     assert.deepEqual(got, want)
+  }),
+  test('_toPairs', () => {
+    const given = { a: 'foo', b: 'bar' }
+    const want = Object.entries(given)
+    const got = mod._toPairs(given)
+    assert.deepEqual(got(), want)
   }),
 
   test('fromEntries: It should call Object.fromEntries', () => {
@@ -134,6 +205,15 @@ const cases = [
     const got = mod.fromEntries(given)
     assert.deepEqual(got, want)
   }),
+  test('_fromEntries', () => {
+    const given = [
+      ['a', 'foo'],
+      ['b', 'bar']
+    ]
+    const want = Object.fromEntries(given)
+    const got = mod._fromEntries(given)
+    assert.deepEqual(got(), want)
+  }),
 
   test('fromPairs: is an alias for fromEntries', () => {
     const given = [
@@ -144,6 +224,15 @@ const cases = [
     const got = mod.fromPairs(given)
     assert.deepEqual(got, want)
   }),
+  test('_fromPairs', () => {
+    const given = [
+      ['a', 'foo'],
+      ['b', 'bar']
+    ]
+    const want = Object.fromEntries(given)
+    const got = mod._fromPairs(given)
+    assert.deepEqual(got(), want)
+  }),
 
   test('assign: It should call Object.assign', () => {
     const o1 = { foo: 1 }
@@ -151,6 +240,13 @@ const cases = [
     const want = Object.assign(o1, o2)
     const got = mod.assign(o1, o2)
     assert.deepEqual(got, want)
+  }),
+  test('_assign', () => {
+    const o1 = { foo: 1 }
+    const o2 = { bar: 2 }
+    const want = Object.assign(o1, o2)
+    const got = mod._assign(o1, o2)
+    assert.deepEqual(got(), want)
   }),
 
   test('mkMerge: It should offload to assign when objects passed', () => {
@@ -208,12 +304,25 @@ const cases = [
     assert.strictEqual(o1.includes(2), true)
     assert.strictEqual(o1.includes(3), true)
   }),
+  test('_mkMerge', () => {
+    const o1 = { foo: 1 }
+    const o2 = { bar: 2 }
+    const want = Object.assign(o1, o2)
+    const got = mod._mkMerge()(o1, o2)
+    assert.deepEqual(got(), want)
+  }),
 
   // Comparison
   // Boolean question
   test('isTrue: It should check if something is "true"', () => {
     const got = mod.isTrue(true)
     assert.strictEqual(got, true)
+  }),
+  test('_isTrue', () => {
+    const got = mod._isTrue(true)
+    assert.strictEqual(typeof got, 'function')
+    const delayedGot = got()
+    assert.strictEqual(delayedGot, true)
   }),
 
   test('isTrue: neg: It should check if something is "true"', () => {
@@ -225,17 +334,14 @@ const cases = [
     const got = mod.isFalse(false)
     assert.strictEqual(got, true)
   }),
+  test('_isFalse', () => {
+    const got = mod._isFalse(false)
+    assert.strictEqual(got(), true)
+  }),
 
   test('isFalse: neg: It should check if something is "false"', () => {
     const got = mod.isFalse(true)
     assert.strictEqual(got, false)
-  }),
-
-  test('_isTrue: ensure make is working', () => {
-    const got = mod._isTrue(true)
-    assert.strictEqual(typeof got, 'function')
-    const delayedGot = got()
-    assert.strictEqual(delayedGot, true)
   }),
 
   // Type
@@ -249,21 +355,38 @@ const cases = [
       mod.isFunction(() => {})
     )
   }),
+  test('_isFunction', () => {
+    assert.strictEqual(true, mod._isFunction(function a () {})())
+    assert.strictEqual(true, mod._isFunction(() => {})())
+  }),
 
   test('isArray: should check if things are arrays', () => {
     assert.strictEqual(true, mod.isArray([]))
     assert.strictEqual(false, mod.isArray({}))
+  }),
+  test('_isArray', () => {
+    assert.strictEqual(true, mod._isArray([])())
+    assert.strictEqual(false, mod._isArray({})())
   }),
 
   test('isString: should check if things are strings', () => {
     assert.strictEqual(true, mod.isString(''))
     assert.strictEqual(false, mod.isString({}))
   }),
+  test('_isString', () => {
+    assert.strictEqual(true, mod._isString('')())
+    assert.strictEqual(false, mod._isString({})())
+  }),
 
   test('isNumber: should check if things are numbers', () => {
     assert.strictEqual(true, mod.isNumber(1))
     assert.strictEqual(false, mod.isNumber('1'))
     assert.strictEqual(false, mod.isNumber({}))
+  }),
+  test('_isNumber', () => {
+    assert.strictEqual(true, mod._isNumber(1)())
+    assert.strictEqual(false, mod._isNumber('1')())
+    assert.strictEqual(false, mod._isNumber({})())
   }),
 
   test('isNull: should check if things are null', () => {
@@ -277,6 +400,14 @@ const cases = [
       mod.isNull(() => {})
     )
   }),
+  test('_isNull', () => {
+    assert.strictEqual(true, mod._isNull(null)())
+    assert.strictEqual(false, mod._isNull(undefined)())
+    assert.strictEqual(false, mod._isNull({})())
+    assert.strictEqual(false, mod._isNull('str')())
+    assert.strictEqual(false, mod._isNull(1)())
+    assert.strictEqual(false, mod._isNull(() => {})())
+  }),
 
   test('length: should return length of "loopable" things', () => {
     assert.strictEqual(1, mod.length([1]))
@@ -284,15 +415,29 @@ const cases = [
     assert.strictEqual(0, mod.length([]))
     assert.strictEqual(0, mod.length({}))
   }),
+  test('_length', () => {
+    assert.strictEqual(1, mod._length([1])())
+    assert.strictEqual(1, mod._length({ a: 1 })())
+    assert.strictEqual(0, mod._length([])())
+    assert.strictEqual(0, mod._length({})())
+  }),
 
   test('isObject: should check if things are objects', () => {
     assert.strictEqual(false, mod.isObject([]))
     assert.strictEqual(true, mod.isObject({}))
   }),
+  test('_isObject', () => {
+    assert.strictEqual(false, mod._isObject([])())
+    assert.strictEqual(true, mod._isObject({})())
+  }),
 
   test('isSymbol: should check if things are symbols', () => {
     assert.strictEqual(true, mod.isSymbol(Symbol()))
     assert.strictEqual(false, mod.isSymbol({}))
+  }),
+  test('_isSymbol', () => {
+    assert.strictEqual(true, mod._isSymbol(Symbol())())
+    assert.strictEqual(false, mod._isSymbol({})())
   }),
 
   test('isUndefined: should check if things are undefineds', () => {
@@ -301,6 +446,12 @@ const cases = [
     assert.strictEqual(false, mod.isUndefined(false))
     assert.strictEqual(false, mod.isUndefined({}))
   }),
+  test('_isUndefined', () => {
+    assert.strictEqual(true, mod._isUndefined(undefined)())
+    assert.strictEqual(false, mod._isUndefined('undefined')())
+    assert.strictEqual(false, mod._isUndefined(false)())
+    assert.strictEqual(false, mod._isUndefined({})())
+  }),
 
   test('isDefined: should be the opposite of `isUndefined`', () => {
     assert.strictEqual(false, mod.isDefined(undefined))
@@ -308,11 +459,21 @@ const cases = [
     assert.strictEqual(true, mod.isDefined(false))
     assert.strictEqual(true, mod.isDefined({}))
   }),
+  test('_isDefined', () => {
+    assert.strictEqual(false, mod._isDefined(undefined)())
+    assert.strictEqual(true, mod._isDefined('undefined')())
+    assert.strictEqual(true, mod._isDefined(false)())
+    assert.strictEqual(true, mod._isDefined({})())
+  }),
 
   // Access
   test('first: should return first item in array', () => {
     const arr = [1, 2, 3]
     assert.strictEqual(1, mod.first(arr))
+  }),
+  test('_first', () => {
+    const arr = [1, 2, 3]
+    assert.strictEqual(1, mod._first(arr)())
   }),
 
   test('first: should return first item in object', () => {
@@ -323,6 +484,10 @@ const cases = [
   test('last: should return last item in array', () => {
     const arr = [1, 2, 3]
     assert.strictEqual(3, mod.last(arr))
+  }),
+  test('_last', () => {
+    const arr = [1, 2, 3]
+    assert.strictEqual(3, mod._last(arr)())
   }),
 
   test('last: should return last item in object', () => {
@@ -337,12 +502,24 @@ const cases = [
     assert.strictEqual(false, mod.isEmpty([1]))
     assert.strictEqual(false, mod.isEmpty({ a: 1 }))
   }),
+  test('_isEmpty', () => {
+    assert.strictEqual(true, mod._isEmpty([])())
+    assert.strictEqual(true, mod._isEmpty({})())
+    assert.strictEqual(false, mod._isEmpty([1])())
+    assert.strictEqual(false, mod._isEmpty({ a: 1 })())
+  }),
 
   test('isNotEmpty: should be opposite of `isEmpty`', () => {
     assert.strictEqual(false, mod.isNotEmpty([]))
     assert.strictEqual(false, mod.isNotEmpty({}))
     assert.strictEqual(true, mod.isNotEmpty([1]))
     assert.strictEqual(true, mod.isNotEmpty({ a: 1 }))
+  }),
+  test('_isNotEmpty', () => {
+    assert.strictEqual(false, mod._isNotEmpty([])())
+    assert.strictEqual(false, mod._isNotEmpty({})())
+    assert.strictEqual(true, mod._isNotEmpty([1])())
+    assert.strictEqual(true, mod._isNotEmpty({ a: 1 })())
   })
 ]
 
